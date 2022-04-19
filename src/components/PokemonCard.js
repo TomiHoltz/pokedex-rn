257e574b3469047c76fb1c4fc80b54a2d7bd0ee9
Tomas Holtz <tomiholtz@gmail.com>
2022-04-19 17:34:27 -0300

    PokeCard

---
+++ src/components/PokemonCard.js
@@ -0,0 +1,66 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TouchableWithoutFeedback,
+} from "react-native";
+
+export default function PokemonCard(props) {
+  const { pokemon } = props;
+
+  const goToPokemon = () => {
+    console.log(`Vamos al pokemon: ${pokemon.name}`);
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={goToPokemon}>
+      <View style={styles.card}>
+        <View style={styles.spacing}>
+          <View style={styles.bgStyles}>
+            <Text style={styles.number}>
+              #{`${pokemon.order}`.padStart(3, 0)}
+            </Text>
+            <Text style={styles.name}>{pokemon.name}</Text>
+            <Image source={{ uri: pokemon.imagen }} style={styles.image} />
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    flex: 1,
+    height: 140,
+  },
+  spacing: {
+    flex: 1,
+    padding: 5,
+  },
+  bgStyles: {
+    backgroundColor: "grey",
+  },
+  number: {
+    position: "absolute",
+    right: 10,
+    top: 10,
+    color: "#fff",
+    fontSize: 11,
+  },
+  name: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 15,
+    paddingTop: 10,
+  },
+  image: {
+    position: "relative",
+    bottom: 2,
+    right: 2,
+    width: 90,
+    height: 90,
+  },
+});
--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,23 +1,24 @@
-import { StyleSheet, Text, FlatList } from 'react-native'
-import React from 'react'
+import React from "react";
+import { StyleSheet, Text, FlatList } from "react-native";
+import PokemonCard from "./PokemonCard";
 
 export default function PokemonList(props) {
-    const { pokemons } = props;
-    return (
-        <FlatList
-            data={pokemons}
-            numColumns={2}
-            showsVerticalScrollIndicator={false}
-            keyExtractor={(pokemon) => String(pokemon.id)}
-            renderItem={({ item }) => <Text>{item.name}</Text>}
-            contentContainerStyle={styles.flatListContentContainer}
+  const { pokemons } = props;
 
-        />
-    )
+  return (
+    <FlatList
+      data={pokemons}
+      numColumns={2}
+      showsVerticalScrollIndicator={false}
+      keyExtractor={(pokemon) => String(pokemon.id)}
+      renderItem={({ item }) => <PokemonCard pokemon={item} />}
+      contentContainerStyle={styles.flatListContentContainer}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
-    flatListContentContainer: {
-        paddingHorizontal: 5
-    }
-})
+  flatListContentContainer: {
+    paddingHorizontal: 5,
+  },
+});
