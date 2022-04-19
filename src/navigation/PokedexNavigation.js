a7a39fc2b74e6bc593d98be0de52efb0a8d9aebd
Tomas Holtz <tomiholtz@gmail.com>
2022-04-19 15:34:00 -0300

    Stack Navigation 2

--- src/navigation/PokedexNavigation.js
+++ src/navigation/PokedexNavigation.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import PokedexScreen from '../screens/Pokedex'
+import PokemonScreen from '../screens/Pokemon'
 
 const Stack = createStackNavigator();
 
 export default function PokedexNavigation() {
     return (
-        <Stack.Navigator>
-            <Stack.Screen name='Favorites' component={PokedexScreen} options={{
+        <Stack.Navigator initialRouteName='Pokedex'>
+            <Stack.Screen name='Pokedex' component={PokedexScreen} options={{
                 title: "Pokedex"
+            }} />
+            <Stack.Screen name='Pokemon' component={PokedexScreen} options={{
+                title: "Pokemon"
             }} />
         </Stack.Navigator>
     )
