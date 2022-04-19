b81cc86d8dac9064d2f91a890d7de92c332627e2
Tomas Holtz <tomiholtz@gmail.com>
2022-04-19 11:30:24 -0300

    Navigation part 1

---
+++ src/screens/Favorites.js
@@ -0,0 +1,10 @@
+import { Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+export default function Favorites() {
+  return (
+    <SafeAreaView>
+      <Text>Favorites</Text>
+    </SafeAreaView>
+  )
+}
